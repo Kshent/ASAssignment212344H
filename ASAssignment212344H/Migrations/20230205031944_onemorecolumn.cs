b362ce40b8a81ec14a143c34d98d64b512c90b2c
Kshent <96482381+Kshent@users.noreply.github.com>
2023-02-06 10:29:29 +0800

    Add project files.

---
+++ ASAssignment212344H/Migrations/20230205031944_onemorecolumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ASAssignment212344H.Migrations
+{
+    /// <inheritdoc />
+    public partial class onemorecolumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Gender",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "AspNetUsers");
+        }
+    }
+}
